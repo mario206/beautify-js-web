3786c076fa86cad06e3e5e06976ff95f51520113
mario206 <mario206>
2020-03-08 16:29:32 +0800

    更新beautifyjs 1.0.4

--- src/demoStr.js
+++ src/demoStr.js
@@ -3,13 +3,23 @@ var a = 1,b = 2,c = 3;
 function s (dd) {   
     if(a) return console.log("1"),bar(),null;
 }
-(function(){
-  console.log("aaa");
-})(a,function(){
+
+
+(function(a,b){
+  console.log("input1= "+a),b();
+})(b,function(){
   console.log("bbb");
-  (function (){
-    console.log("ccc");
-  })(a);
+  (function (a){
+    console.log("intput2 a = " + a);
+  })(c);
 });
+
+function f(a){
+   var b = function(a){
+      console.log(a);  
+    };
+    b(0);
+}
+
 exports.log = s;
 `;
